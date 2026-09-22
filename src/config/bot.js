778a8cf69af4169f0116b1e6ece17d6c3476a5e9
FiZcc <chungus0709@gmail.com>
2026-09-22 16:24:39 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Bizarre Buckaroos Server", // required by Discord API, not shown in the client
+        name: "Custom Status", // required by Discord API, not shown in the client
         state: "Moderation and Management. Engineered by Arana.",     // this is what people actually see
-        type: 3,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
