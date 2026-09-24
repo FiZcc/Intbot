df5dad271ad3add6a8be78c64af9d1062bb68ea5
FiZcc <chungus0709@gmail.com>
2026-09-23 18:23:28 -0700

    Update default application question

--- src/config/bot.js
+++ src/config/bot.js
@@ -61,7 +61,7 @@ export const botConfig = {
   applications: {
     // Default questions shown when someone fills out an application.
     defaultQuestions: [
-      { question: "What is your name?", required: true },
+      { question: "What is your experience?", required: true },
       { question: "How old are you?", required: true },
       { question: "Why do you want to join?", required: true },
     ],
